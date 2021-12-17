7818279a26d4f9538eae2fe22af475803b565f16
Philip Clegg <phil@mirrorweb.com>
2021-12-17 11:54:44 +0000

    feat: Force PYWB to replay latest version of an archive

--- pywb/apps/rewriterapp.py
+++ pywb/apps/rewriterapp.py
@@ -764,7 +764,7 @@ class RewriterApp(object):
         elif wb_url.is_continuity_replay():
             closest = 'now'
         else:
-            closest = wb_url.timestamp
+            closest = 'now'
 
         params = {'url': wb_url.url, 'closest': closest, 'matchType': 'exact'}
 
